edf62cc6945ac2dc3416934431c43a4acf6febad
Victor Marques <vhm.marques@hotmail.com>
2021-04-04 14:36:55 -0300

    Primeira interação e teste com os controladores e o razor pages

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App para exercitar a linguagem C#";
+            ViewData["developer"] = "Victor Marques";
 
             return View();
         }
